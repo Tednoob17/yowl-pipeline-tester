5fd55b838d8b03099b67834e647e5a9817b6c35a
karimlecalligraphe <karim.bello-yessoufou@epitech.eu>
2024-07-31 18:39:04 +0100

    frontend_Design

---
+++ YOWL-Frontend/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <main>
+    <v-icon icon="mdi-delete"></v-icon>
+    <v-btn append-icon="mdi-send" variant="text">Neciutku</v-btn>
+
+    <h1 class="tw-text-3xl tw-bg-red-600 tw-font-bold tw-underline">Hello world!</h1>
+  </main>
+</template>
