ad3f9da3c745220ef82329e169eecb2b83677898
= <=>
2024-07-31 16:12:06 +0100

    Installing dependencies in the right order
    - laravel scout
    - Laravel socialite
    - Dedoc scramble
    - Laravel reverb
    - Laravel jetstream
    - Spatie laravel permission

--- yowl-backend/app/Models/User.php
+++ yowl-backend/app/Models/User.php
@@ -6,10 +6,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Jetstream\HasTeams;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
+// use Laravel\Scout\Searchable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use HasTeams;
+    use Notifiable;
+    use HasRoles;
+    use TwoFactorAuthenticatable;
+    // use Searchable;
 
     /**
      * The attributes that are mass assignable.
@@ -30,6 +44,17 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array<int, string>
+     */
+    protected $appends = [
+        'profile_photo_url',
     ];
 
     /**
