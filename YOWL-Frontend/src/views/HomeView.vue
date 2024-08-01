809c2313e189c6457893e23c237546831c9f85b6
Ted Kouhouenou <ted.kouhouenou@epitech.eu>
2024-08-01 09:46:54 +0100

    Add expansions panels for Home page

--- YOWL-Frontend/src/views/HomeView.vue
+++ YOWL-Frontend/src/views/HomeView.vue
@@ -1,10 +1,17 @@
 <script setup></script>
 
 <template>
-  <main>
-    <v-icon icon="mdi-delete"></v-icon>
-    <v-btn append-icon="mdi-send" variant="text">Neciutku</v-btn>
 
-    <h1 class="tw-text-3xl tw-bg-red-600 tw-font-bold tw-underline">Hello world!</h1>
-  </main>
+<div>
+  <v-card class="mx-left" width="256">
+
+  <v-expansion-panels >
+
+    <v-expansion-panel title="kikoherrsc@gmail.com">
+    </v-expansion-panel>
+  </v-expansion-panels>
+</v-card>
+</div>
+
+
 </template>
