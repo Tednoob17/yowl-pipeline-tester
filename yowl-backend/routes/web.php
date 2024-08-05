cf283898dd0e90a178e49ec56ab73a38b25b1d38
Ted Kouhouenou <ted.kouhouenou@epitech.eu>
2024-08-05 23:58:07 +0100

    Change Docker-compose config'

--- yowl-backend/routes/web.php
+++ yowl-backend/routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Laravel\Socialite\Facades\Socialite;
+
 
 Route::get("/", function () {
     return Inertia::render("Welcome", [
